cd7f433310067a5fb4c36e8d64cd222e36036bd7
Alex <alexkhotenoff@gmail.com>
2020-12-14 23:23:03 +0300

    add modals

--- js/main.js
+++ js/main.js
@@ -57,6 +57,14 @@ $(document).ready(function () {
             $('.lazy').Lazy();
         }
     }
+    $("input[name='phone_number']").mask("+7 999 999-9999", { autoclear: false });
+    AjaxForm.initialize({
+        "assetsUrl": "\/assets\/components\/ajaxform\/",
+        "actionUrl": "\/assets\/components\/ajaxform\/action.php",
+        "closeMessage": "",
+        "formSelector": "form.ajax_form",
+        "pageId": 1
+    });
     var owl = $(".owl-carousel").owlCarousel(
         {
             loop: true,
@@ -76,5 +84,81 @@ $(document).ready(function () {
             })
         }
     }
-
 });
+
+var sent_message_button = document.querySelector('.main-footer__sent-message');
+var repair_request_button = document.querySelector('.header__button_repair');
+var evacuate_button = document.querySelector('.header__button_evacuator');
+var callback_button = document.querySelector('.header__callback');
+// var service_request_button = document.querySelector('.calc-form__request-button');
+
+if (sent_message_button) {
+    modal_show(sent_message_button, '.modal_sent-message');
+}
+
+if (repair_request_button) {
+    modal_show(repair_request_button, '.modal_repair-request');
+}
+
+if (evacuate_button) {
+    modal_show(evacuate_button, '.modal_evacuate');
+}
+
+if (callback_button) {
+    modal_show(callback_button, '.modal_callback');
+}
+
+function modal_accepted() {
+    var modal = document.querySelector('.modal_accept');
+
+    if (modal) {
+        if (!modal.classList.contains('modal_show')) {
+            modal.classList.add('modal_show');
+            modal_init();
+        }
+    }
+}
+// if (service_request_button) {
+//     modal_show(service_request_button, '.modal_service-request');
+// }
+
+function modal_show(open_button, modal_window_class) {
+    open_button.addEventListener('click', function () {
+        var modal = document.querySelector(modal_window_class);
+
+        if (modal) {
+            if (!modal.classList.contains('modal_show')) {
+                modal.classList.add('modal_show');
+                modal_init();
+            }
+        }
+    });
+}
+
+function modal_init() {
+    let active_modal = document.querySelector('.modal_show');
+
+    if (active_modal) {
+        var cancel_button = active_modal.querySelector('.modal__button_cancel');
+
+        if (cancel_button) {
+            cancel_button.addEventListener('click', function () {
+                modal_close(active_modal);
+            });
+        }
+    }
+
+    window.addEventListener("keydown", function (evt) {
+
+        if (evt.code === "Escape") {
+            evt.preventDefault();
+            modal_close(active_modal);
+        }
+    });
+}
+
+function modal_close(modal_window) {
+    if (modal_window.classList.contains('modal_show')) {
+        modal_window.classList.remove('modal_show');
+    }
+}
